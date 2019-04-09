886af2f6a268e33aabd5eeadffeb731251c0d438
eliasedwin7 <eliasedwin7@gmail.com>
2019-04-09 12:14:02 +0530

    Packet tracer

--- file/Server.java
+++ file/Server.java
@@ -21,27 +21,5 @@ public class Server {
         }
         PrintWriter  out = new PrintWriter(s.getOutputStream(),true);
         BufferedReader br1 = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        String fileName = br1.readLine();
-        File f = new File(fileName);
-        if (f.exists()) {
-            long pid = Long.parseLong(java.lang.management.ManagementFactory.getRuntimeMXBean().getName().split("@")[0]);
-            out.write("Process id is:"+pid+"\nFile contents are: ");
-            BufferedReader in = new BufferedReader(new FileReader(fileName));
-            String v;
-            while ((v = in.readLine()) != null) {
-                out.write(v);
-                out.flush();
-            }
-            in.close();
-            System.out.println("File served");
-        }
-        else{
-            out.write("File not found");
-            out.flush();
-            System.out.println("File not found");
-        }
-        s.close();
-        ss.close();
-    }
-}
-}
+
+    
